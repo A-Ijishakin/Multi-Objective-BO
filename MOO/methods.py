c72ac2ed166a6b4929b06b50b0433201aed03e5d
A-Ijishakin <ayodeji.ijishakin.21@ucl.ac.uk>
2024-06-19 10:33:12 +0100

    Munich MOO

--- MOO/methods.py
+++ MOO/methods.py
@@ -6,7 +6,6 @@ from botorch.models.model_list_gp_regression import ModelListGP
 from botorch.models.transforms.outcome import Standardize
 from gpytorch.mlls.sum_marginal_log_likelihood import SumMarginalLogLikelihood
 from botorch.utils.transforms import unnormalize, normalize
-
 from botorch.optim.optimize import optimize_acqf, optimize_acqf_list
 from botorch.acquisition.objective import GenericMCObjective
 from botorch.utils.multi_objective.scalarization import get_chebyshev_scalarization
@@ -17,10 +16,13 @@ from botorch.acquisition.multi_objective.monte_carlo import (
     qExpectedHypervolumeImprovement,
     qNoisyExpectedHypervolumeImprovement,
 )
+import numpy as np 
 from botorch.utils.sampling import sample_simplex
 from botorch.acquisition.monte_carlo import qNoisyExpectedImprovement
 import warnings
 from botorch.exceptions import BadInitialCandidatesWarning
+from gpytorch.mlls import ExactMarginalLogLikelihood 
+from sklearn.preprocessing import StandardScaler
 warnings.filterwarnings("ignore", message="Attempted to use direct, but fortran library could not be imported. Using PDOO optimiser instead of direct.")
 
 tkwargs = {
@@ -37,12 +39,12 @@ SMOKE_TEST = True
 
 problem = BraninCurrin(negate=True).to(**tkwargs)
 
-BATCH_SIZE = 2 
+# BATCH_SIZE = 2 
 NUM_RESTARTS = 10 if not SMOKE_TEST else 3
 RAW_SAMPLES = 512 if not SMOKE_TEST else 4
 
-standard_bounds = torch.zeros(2, problem.dim, **tkwargs)
-standard_bounds[1] = 1
+standard_bounds = torch.ones(2, problem.dim, **tkwargs) * -1 
+standard_bounds[1] = 0
 
 warnings.filterwarnings("ignore", category=BadInitialCandidatesWarning)
 warnings.filterwarnings("ignore", category=RuntimeWarning)
@@ -52,6 +54,47 @@ MC_SAMPLES = 128 if not SMOKE_TEST else 16
 
 verbose = True
 
+def produce_mean_and_variance(train_x, train_obj):
+    # Compute the mean and variance along the dimension 1 (groups of three)
+    train_obj_mean = np.mean(train_obj.reshape(-1, 3, 2), axis=1) 
+    train_x = np.mean(train_x.reshape(-1, 3, 20), axis=1)  
+
+    #import the standard scalers
+    xScaler, yScaler = StandardScaler(), StandardScaler()
+
+    #scale the data 
+    train_x = torch.tensor(xScaler.fit_transform(train_x).astype(np.float32)) 
+    train_obj_mean = torch.tensor(yScaler.fit_transform(train_obj_mean).astype(np.float32)) * -1 
+
+    Yvar = torch.tensor(np.mean(np.var(train_obj.reshape(-1, 3, 2), axis=1)).astype(np.float32)) 
+    return train_x, train_obj_mean, Yvar, xScaler, yScaler
+
+def add_noise(data, train_y, noise_factor=0.1): 
+    # Calculate mean and standard deviation for each column
+    # means = data.mean(dim=0)
+    stds = np.std(data, axis=0) 
+
+    # Generate Gaussian noise scaled by the standard deviation of each column
+    noise = torch.randn_like(torch.tensor(data.astype(np.float32))) * (stds * noise_factor)
+
+    # Add noise to the original data
+    noisy_data = data + np.array(noise)   
+
+    xScaler, yScaler = StandardScaler(), StandardScaler()
+
+    noisy_data = torch.tensor(xScaler.fit_transform(noisy_data).astype(np.float32)) 
+    train_y = torch.tensor(yScaler.fit_transform(train_y).astype(np.float32)) * -1 
+
+    return noisy_data, train_y, xScaler, yScaler
+
+
+def denormalize_rows(normalized_tensor, row_min, row_range):
+    """
+    Denormalizes each row of a tensor from the range [0, 1] back to the original range.
+    """
+    original_tensor = normalized_tensor * row_range + row_min
+    return original_tensor
+
 def generate_initial_data(n=6):
     # generate training data
     train_x = draw_sobol_samples(bounds=problem.bounds, n=n, q=1).squeeze(1)
@@ -60,91 +103,106 @@ def generate_initial_data(n=6):
     return train_x, train_obj, train_obj_true
 
 
-def initialize_model(train_x, train_obj):
-    # define models for objective and constraint
-    train_x = normalize(train_x, problem.bounds)
+def initialize_model(train_x, train_obj, train_yvar=None):
     models = []
     for i in range(train_obj.shape[-1]):
         train_y = train_obj[..., i : i + 1]
-        train_yvar = torch.full_like(train_y, NOISE_SE[i] ** 2)
         models.append(
             FixedNoiseGP(
-                train_x, train_y, train_yvar, outcome_transform=Standardize(m=1)
-            )
+                train_X=train_x, train_Y=train_y, train_Yvar=train_yvar,
+            ) 
         )
     model = ModelListGP(*models)
     mll = SumMarginalLogLikelihood(model.likelihood, model)
-    return mll, model
+    return mll, model 
 
 
-def optimize_qehvi_and_get_observation(model, train_x, train_obj, sampler):
-    """Optimizes the qEHVI acquisition function, and returns a new candidate and observation."""
-    # partition non-dominated space into disjoint rectangles
-    with torch.no_grad():
-        pred = model.posterior(normalize(train_x, problem.bounds)).mean
-    partitioning = FastNondominatedPartitioning(
-        ref_point=problem.ref_point,
-        Y=pred,
-    )
-    acq_func = qExpectedHypervolumeImprovement(
-        model=model,
-        ref_point=problem.ref_point,
-        partitioning=partitioning,
-        sampler=sampler,
-    )
-    # optimize
-    candidates, _ = optimize_acqf(
-        acq_function=acq_func,
-        bounds=standard_bounds,
-        q=BATCH_SIZE,
-        num_restarts=NUM_RESTARTS,
-        raw_samples=RAW_SAMPLES,  # used for intialization heuristic
-        options={"batch_limit": 5, "maxiter": 200},
-        sequential=True,
-    )
-    # observe new values
-    new_x = unnormalize(candidates.detach(), bounds=problem.bounds)
-    new_obj_true = problem(new_x)
-    new_obj = new_obj_true + torch.randn_like(new_obj_true) * NOISE_SE
-    return new_x, new_obj, new_obj_true
+def calc_likelihoods(model, train_x, train_obj):
+    likelihoods = []
+    # Assuming train_obj has the same order of objectives as models in ModelListGP
+    for i, single_model in enumerate(model.models):
+        # Set model and its likelihood to evaluation mode
+        single_model.eval()
+        single_model.likelihood.eval()
+
+        with torch.no_grad():
+            # Get the output from the model
+            output = single_model(train_x)
+            # We assume train_obj is already appropriately sliced per model
+            train_y = train_obj[..., i:i+1]
+            # Initialize the Exact Marginal Log Likelihood for the single model
+            mll = ExactMarginalLogLikelihood(single_model.likelihood, single_model)
+            # Calculate the MLL
+            mll_value = mll(output, train_y)
+            # Since we generally minimize the negative MLL, we display the negative of mll_value
+            likelihoods.append(mll_value.sum().item()) 
+    return np.mean(likelihoods)  
 
 
-def optimize_qnehvi_and_get_observation(model, train_x, train_obj, sampler):
-    """Optimizes the qEHVI acquisition function, and returns a new candidate and observation."""
-    # partition non-dominated space into disjoint rectangles
-    acq_func = qNoisyExpectedHypervolumeImprovement(
-        model=model,
-        ref_point=problem.ref_point.tolist(),  # use known reference point
-        X_baseline=normalize(train_x, problem.bounds),
-        prune_baseline=True,  # prune baseline points that have estimated zero probability of being Pareto optimal
-        sampler=sampler,
-    )
-    # optimize
-    candidates, _ = optimize_acqf(
-        acq_function=acq_func,
-        bounds=standard_bounds,
-        q=BATCH_SIZE,
-        num_restarts=NUM_RESTARTS,
-        raw_samples=RAW_SAMPLES,  # used for intialization heuristic
-        options={"batch_limit": 5, "maxiter": 200},
-        sequential=True,
-    )
-    # observe new values
-    new_x = unnormalize(candidates.detach(), bounds=problem.bounds)
-    new_obj_true = problem(new_x)
-    new_obj = new_obj_true + torch.randn_like(new_obj_true) * NOISE_SE
-    return new_x, new_obj, new_obj_true
+# def optimize_qehvi_and_get_observation(model, train_x, train_obj, sampler, BATCH_SIZE=2):
+#     """Optimizes the qEHVI acquisition function, and returns a new candidate and observation."""
+#     # partition non-dominated space into disjoint rectangles
+#     with torch.no_grad():
+#         pred = model.posterior(normalize(train_x, problem.bounds)).mean
+#     partitioning = FastNondominatedPartitioning(
+#         ref_point=problem.ref_point,
+#         Y=pred,
+#     )
+#     acq_func = qExpectedHypervolumeImprovement(
+#         model=model,
+#         ref_point=problem.ref_point,
+#         partitioning=partitioning,
+#         sampler=sampler,
+#     )
+#     # optimize
+#     candidates, _ = optimize_acqf(
+#         acq_function=acq_func,
+#         bounds=standard_bounds,
+#         q=BATCH_SIZE,
+#         num_restarts=NUM_RESTARTS,
+#         raw_samples=RAW_SAMPLES,  # used for intialization heuristic
+#         options={"batch_limit": 5, "maxiter": 200},
+#         sequential=True,
+#     )
+#     # observe new values
+#     new_x = unnormalize(candidates.detach(), bounds=problem.bounds)
+    
+#     return new_x 
 
 
-def optimize_qnparego_and_get_observation(model, train_x, train_obj, sampler):
+# # def optimize_qnehvi_and_get_observation(model, train_x, train_obj, sampler, BATCH_SIZE=2):
+# #     """Optimizes the qEHVI acquisition function, and returns a new candidate and observation."""
+# #     # partition non-dominated space into disjoint rectangles
+#     acq_func = qNoisyExpectedHypervolumeImprovement(
+#         model=model,
+#         # ref_point=problem.ref_point.tolist(),  # use known reference point
+#         X_baseline=train_x,
+#         prune_baseline=True,  # prune baseline points that have estimated zero probability of being Pareto optimal
+#         sampler=sampler,
+#     )
+#     # optimize
+#     candidates, _ = optimize_acqf(
+#         acq_function=acq_func,
+#         bounds=standard_bounds,
+#         q=BATCH_SIZE,
+#         num_restarts=NUM_RESTARTS,
+#         raw_samples=RAW_SAMPLES,  # used for intialization heuristic
+#         options={"batch_limit": 5, "maxiter": 200},
+#         sequential=True,
+#     )
+
+#     return candidates  
+
+
+def optimize_qnparego_and_get_observation(model, train_x, sampler, BATCH_SIZE=2):
     """Samples a set of random weights for each candidate in the batch, performs sequential greedy optimization
     of the qNParEGO acquisition function, and returns a new candidate and observation."""
-    train_x = normalize(train_x, problem.bounds)
     with torch.no_grad():
         pred = model.posterior(train_x).mean
     acq_func_list = []
+
     for _ in range(BATCH_SIZE):
-        weights = sample_simplex(problem.num_objectives, **tkwargs).squeeze()
+        weights = sample_simplex(2, **tkwargs).squeeze()
         objective = GenericMCObjective(
             get_chebyshev_scalarization(weights=weights, Y=pred)
         )
@@ -156,6 +214,12 @@ def optimize_qnparego_and_get_observation(model, train_x, train_obj, sampler):
             prune_baseline=True,
         )
         acq_func_list.append(acq_func)
+    
+    lower = torch.min(train_x, dim=0).values 
+    upper = torch.max(train_x, dim=0).values 
+
+    standard_bounds = torch.stack([lower, upper], dim=0) 
+    
     # optimize
     candidates, _ = optimize_acqf_list(
         acq_function_list=acq_func_list,
@@ -165,7 +229,8 @@ def optimize_qnparego_and_get_observation(model, train_x, train_obj, sampler):
         options={"batch_limit": 5, "maxiter": 200},
     )
     # observe new values
-    new_x = unnormalize(candidates.detach(), bounds=problem.bounds)
-    new_obj_true = problem(new_x)
-    new_obj = new_obj_true + torch.randn_like(new_obj_true) * NOISE_SE
-    return new_x, new_obj, new_obj_true 
+    return candidates  
+
+
+
+
--- MOO/multi_objective_dragon.py
+++ MOO/multi_objective_dragon.py
@@ -1,257 +1,77 @@
 import torch
-import time
-from botorch import fit_gpytorch_mll
-from botorch.utils.multi_objective.box_decompositions.dominated import (
-    DominatedPartitioning,
-)
+from botorch import fit_gpytorch_mll 
+from botorch.models.transforms import Normalize, Standardize 
 from botorch.sampling.normal import SobolQMCNormalSampler
-from dragonfly import load_config
-from argparse import Namespace
-from dragonfly.exd.experiment_caller import CPMultiFunctionCaller
-from dragonfly.opt.multiobjective_gp_bandit import CPMultiObjectiveGPBandit 
-from dragonfly.exd.worker_manager import SyntheticWorkerManager
-from methods import (generate_initial_data, initialize_model, problem, 
-                     optimize_qehvi_and_get_observation, optimize_qnehvi_and_get_observation, 
-                      optimize_qnparego_and_get_observation, 
-                      MC_SAMPLES, BATCH_SIZE, NOISE_SE, N_BATCH, verbose, N_TRIALS) 
+from methods import (initialize_model, produce_mean_and_variance, 
+                    calc_likelihoods, 
+                    optimize_qnparego_and_get_observation, add_noise, 
+                    MC_SAMPLES) 
 import numpy as np 
-import json 
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler 
+
+#set the seed for reproducibility 
+torch.manual_seed(42)
+np.random.seed(42) 
+
+#set the batch size 
+BATCH_SIZE = 32 
+
+#load in the data 
+path = 'WSSB MasterFile.xlsx'
+
+#load in the data 
+data = pd.read_excel(path) 
+
+#specify the training data and objective values 
+train_X  = data.iloc[:96].drop(['Campaign', 'date', 'sample_id', 'replica_id', 'exp_id', 'well', 'Growth (OD)', 'Lipid content (FL)'], axis=1).values
+train_Y = data.iloc[:96][['Growth (OD)', 'Lipid content (FL)']].values  
+
+#memory allocation for the likelihoods and candidates 
+likelihoods, all_candidates = [], []
+
+for i in range(3): 
+    train_obj = train_Y 
+    if i == 0:
+        train_x, train_obj, train_Yvar, xScaler, yScaler = produce_mean_and_variance(train_X, train_Y)  
+    elif i == 1: 
+        train_x, train_obj, xScaler, yScaler = add_noise(train_X, train_Y) 
+    else:
+        xScaler, yScaler = StandardScaler(), StandardScaler()
+        ids = np.array([0.2, 0.5, 0.8] * 32) 
+        train_x = np.concatenate([train_X, ids.reshape(96, 1)], axis=1) 
+        train_x = torch.tensor(xScaler.fit_transform(train_X).astype(np.float32))
+        train_obj = torch.tensor(yScaler.fit_transform(train_Y).astype(np.float32)) * -1 
+
+    #initialize model 
+    mll_qnparego, model_qnparego = initialize_model(train_x, train_obj) 
+
+    #fit the gp modell
+    fit_gpytorch_mll(mll_qnparego) 
+
+    likelihoods.append(calc_likelihoods(model_qnparego, train_x, train_obj)) 
+
+    #initialize the sampler
+    qnparego_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([MC_SAMPLES]))  
+
+    #get candidates 
+    candidates = optimize_qnparego_and_get_observation(model=model_qnparego, train_x=train_x, 
+                                                        sampler=qnparego_sampler, BATCH_SIZE=BATCH_SIZE)
+
+    #denormalize the candidates
+    candidates = xScaler.inverse_transform(np.array(candidates).astype(np.float64))   
+
+    all_candidates.append(candidates)
+
+candidate_index = np.argmax(likelihoods) 
+candidates = all_candidates[candidate_index]    
+
+columns = data.drop(['Campaign', 'date', 'sample_id', 'replica_id', 'exp_id', 'well', 'Growth (OD)', 'Lipid content (FL)'], axis=1).columns 
+results = pd.DataFrame({columns[i]: candidates[:, i] for i in range(len(columns))}) 
+
+results.to_excel('Results.xlsx')
 
 
-torch.manual_seed(43)
-np.random.seed(43) 
 
-hvs_all_qparego, hvs_all_qehvi, hvs_all_qnehvi, hvs_all_random, hvs_all_dragonfly = [], [], [], [], [] 
-train_obj_true_all_qparego, train_obj_true_all_qehvi, train_obj_true_all_qnehvi, train_obj_true_all_random, train_obj_true_all_dragonfly = [], [], [], [], [] 
-train_obj_all_qparego, train_obj_all_qehvi, train_obj_all_qnehvi, train_obj_all_random, train_obj_all_dragonfly = [], [], [], [], [] 
-train_x_all_qparego, train_x_all_qehvi, train_x_all_qnehvi, train_x_all_random, train_x_all_dragonfly = [], [], [], [], []
+  
 
-
-for n in N_TRIALS:
-    options = Namespace(
-                # build_new_model_every=BATCH_SIZE,  
-                total_budget = 100, 
-                
-                # set to batch size
-                # init_capital=2,  # number of initialization experiments
-                # (-1 is included since Dragonfly generates n+1 experiments)
-                gpb_hp_tune_criterion='ml-post_sampling',  # Criterion for tuning GP hyper-parameters.
-                # Options: 'ml-post_sampling' (algorithm default), 'ml', 'post_sampling'.
-                moors_scalarisation='linear',  # Scalarization approach for multi-objective opt. 'linear' or 'tchebychev'
-                acq_opt_max_evals=1,
-                # acq_opt_method = 'bo'
-
-            )
-    # Create optimizer object
-    config_params = {
-        'domain': [{'type': 'float', 'min': 0.0, 'max': 1.0},
-                {'type': 'float', 'min': 0.0, 'max': 1.0}]
-    }
-    config = load_config(config_params)
-
-    func_caller = CPMultiFunctionCaller(None, config.domain,
-                                        domain_orderings=config.domain_orderings)
-    func_caller.num_funcs = 2  # must specify how many functions are being optimized
-
-    wm = SyntheticWorkerManager(1)
-    dragonfly_opt = CPMultiObjectiveGPBandit(func_caller, wm, options=options)
-    dragonfly_opt.ask_tell_mode = True
-    dragonfly_opt.worker_manager = None
-    dragonfly_opt._set_up()
-    # dragonfly_opt._build_new_model()
-    # dragonfly_opt._set_next_gp()
-    dragonfly_opt.initialise()
-    dragonfly_opt.ask()
-
-    hvs_qparego, hvs_qehvi, hvs_qnehvi, hvs_random, hvs_dragonfly = [], [], [], [], []  
-    
-    # call helper functions to generate initial training data
-    train_x_qparego, train_obj_qparego, train_obj_true_qparego = generate_initial_data(
-        n=6
-    )
-    train_x_qehvi, train_obj_qehvi, train_obj_true_qehvi = (
-        train_x_qparego,
-        train_obj_qparego,
-        train_obj_true_qparego,
-    )
-    train_x_qnehvi, train_obj_qnehvi, train_obj_true_qnehvi = (
-        train_x_qparego,
-        train_obj_qparego,
-        train_obj_true_qparego,
-    )
-    train_x_random, train_obj_random, train_obj_true_random = (
-        train_x_qparego,
-        train_obj_qparego,
-        train_obj_true_qparego,
-    )
-
-    train_x_dragonfly, train_obj_dragonfly, train_obj_true_dragonfly = (
-        train_x_qparego,
-        train_obj_qparego,
-        train_obj_true_qparego
-    )
-
-    # new_x_dragonfly, new_obj_dragonfly = train_x_dragonfly, train_obj_dragonfly
-
-    #initialize models for the different acquisition functions
-    mll_qparego, model_qparego = initialize_model(train_x_qparego, train_obj_qparego)
-    mll_qehvi, model_qehvi = initialize_model(train_x_qehvi, train_obj_qehvi)
-    mll_qnehvi, model_qnehvi = initialize_model(train_x_qnehvi, train_obj_qnehvi)
-
-
-    # compute hypervolume
-    bd = DominatedPartitioning(ref_point=problem.ref_point, Y=train_obj_true_qparego)
-    volume = bd.compute_hypervolume().item()
-
-    hvs_qparego.append(volume)
-    hvs_qehvi.append(volume)
-    hvs_qnehvi.append(volume)
-    hvs_random.append(volume)
-    hvs_dragonfly.append(volume)
-
-    # run N_BATCH rounds of BayesOpt after the initial random batch
-    for iteration in range(1, N_BATCH + 1):
-        t0 = time.monotonic()
-
-        # fit the models
-        fit_gpytorch_mll(mll_qparego)
-        fit_gpytorch_mll(mll_qehvi)
-        fit_gpytorch_mll(mll_qnehvi)
-
-        # define the qEI and qNEI acquisition modules using a QMC sampler
-        qparego_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([MC_SAMPLES]))
-        qehvi_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([MC_SAMPLES]))
-        qnehvi_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([MC_SAMPLES]))
-
-        # optimize acquisition functions and get new observations
-        (
-            new_x_qparego,
-            new_obj_qparego,
-            new_obj_true_qparego,
-        ) = optimize_qnparego_and_get_observation(
-            model_qparego, train_x_qparego, train_obj_qparego, qparego_sampler
-        )
-        new_x_qehvi, new_obj_qehvi, new_obj_true_qehvi = optimize_qehvi_and_get_observation(
-            model_qehvi, train_x_qehvi, train_obj_qehvi, qehvi_sampler
-        )
-        (
-            new_x_qnehvi,
-            new_obj_qnehvi,
-            new_obj_true_qnehvi,
-        ) = optimize_qnehvi_and_get_observation(
-            model_qnehvi, train_x_qnehvi, train_obj_qnehvi, qnehvi_sampler
-        )
-        new_x_random, new_obj_random, new_obj_true_random = generate_initial_data(
-            n=BATCH_SIZE
-        )
-
-        for (x, y) in zip(train_x_dragonfly, train_obj_dragonfly):
-            #dragonfly
-            x = [datum.item() for datum in x]
-            y = [datum.item() for datum in y]
-            dragonfly_opt.tell([(x,
-                                y)])
-
-        dragonfly_opt.step_idx += 1
-
-        # Retrieve the Pareto-optimal points
-        new_x_dragonfly = torch.tensor(dragonfly_opt.ask(n_points=BATCH_SIZE)).to('cuda')
-
-        dragonfly_opt._build_new_model()
-        dragonfly_opt._set_next_gp()
-
-        #compute
-        new_obj_true_dragonfly = problem(new_x_dragonfly).to('cuda')
-        new_obj_dragonfly = new_obj_true_dragonfly + torch.randn_like(new_obj_true_dragonfly) * NOISE_SE
-
-        # update training points
-        train_x_qparego = torch.cat([train_x_qparego, new_x_qparego])
-        train_obj_qparego = torch.cat([train_obj_qparego, new_obj_qparego])
-        train_obj_true_qparego = torch.cat([train_obj_true_qparego, new_obj_true_qparego])
-
-        train_x_qehvi = torch.cat([train_x_qehvi, new_x_qehvi])
-        train_obj_qehvi = torch.cat([train_obj_qehvi, new_obj_qehvi])
-        train_obj_true_qehvi = torch.cat([train_obj_true_qehvi, new_obj_true_qehvi])
-
-        train_x_qnehvi = torch.cat([train_x_qnehvi, new_x_qnehvi])
-        train_obj_qnehvi = torch.cat([train_obj_qnehvi, new_obj_qnehvi])
-        train_obj_true_qnehvi = torch.cat([train_obj_true_qnehvi, new_obj_true_qnehvi])
-
-        train_x_dragonfly = torch.cat([train_x_dragonfly, new_x_dragonfly.reshape(-1, 2)])
-        train_obj_dragonfly = torch.cat([train_obj_dragonfly, new_obj_dragonfly.reshape(-1, 2)])
-        train_obj_true_dragonfly = torch.cat([train_obj_true_dragonfly, new_obj_true_dragonfly.reshape(-1, 2)])
-
-        train_x_random = torch.cat([train_x_random, new_x_random])
-        train_obj_random = torch.cat([train_obj_random, new_obj_random])
-        train_obj_true_random = torch.cat([train_obj_true_random, new_obj_true_random])
-
-        # update progress
-        for hvs_list, train_obj in zip(
-            (hvs_random, hvs_qparego, hvs_qehvi, hvs_qnehvi, hvs_dragonfly),
-            (
-                train_obj_true_random,
-                train_obj_true_qparego,
-                train_obj_true_qehvi,
-                train_obj_true_qnehvi,
-                train_obj_true_dragonfly
-            ),
-        ):
-            # compute hypervolume
-            bd = DominatedPartitioning(ref_point=problem.ref_point, Y=train_obj)
-            volume = bd.compute_hypervolume().item()
-            hvs_list.append(volume)
-
-        # reinitialize the models so they are ready for fitting on next iteration
-        # Note: we find improved performance from not warm starting the model hyperparameters
-        # using the hyperparameters from the previous iteration
-        mll_qparego, model_qparego = initialize_model(train_x_qparego, train_obj_qparego)
-        mll_qehvi, model_qehvi = initialize_model(train_x_qehvi, train_obj_qehvi)
-        mll_qnehvi, model_qnehvi = initialize_model(train_x_qnehvi, train_obj_qnehvi)
-
-        # dragonfly_opt._build_new_model()
-        # dragonfly_opt._set_next_gp()
-
-        t1 = time.monotonic()
-        if verbose:
-            print(
-                f"\nBatch {iteration:>2}: Hypervolume (random, qNParEGO, qEHVI, qNEHVI, dragonfly) = "
-                f"({hvs_random[-1]:>4.2f}, {hvs_qparego[-1]:>4.2f}, {hvs_qehvi[-1]:>4.2f}, {hvs_qnehvi[-1]:>4.2f}, {hvs_dragonfly[-1]:>4.2f}), "
-                f"time = {t1-t0:>4.2f}.",
-                end="",
-            )
-        else:
-            print(".", end="")  
-
-    hvs_all_dragonfly.append(hvs_dragonfly), hvs_all_qehvi.append(hvs_qehvi), hvs_all_qnehvi.append(hvs_qnehvi) 
-    hvs_all_qparego.append(hvs_qparego), hvs_all_random.append(hvs_random) 
-    
-    train_obj_true_all_qparego.append(train_obj_true_qparego), train_obj_true_all_qehvi.append(train_obj_true_qehvi) 
-    train_obj_true_all_qnehvi.append(train_obj_true_qnehvi), train_obj_true_all_random.append(train_obj_true_random) 
-    train_obj_true_all_dragonfly.append(train_obj_true_dragonfly) 
-    
-    
-    train_obj_all_qparego.append(train_obj_qparego), train_obj_all_qehvi.append(train_obj_qehvi), 
-    train_obj_all_qnehvi.append(train_obj_qnehvi), train_obj_all_random.append(train_obj_random), 
-    train_obj_all_dragonfly.append(train_obj)
-    
-    train_x_all_qparego.append(train_x_qparego), train_x_all_qehvi.append(train_x_qehvi), 
-    train_x_all_qnehvi.append(train_x_qnehvi), train_x_all_random.append(train_x_random), 
-    train_x_all_dragonfly.append(train_x_dragonfly) 
-
-qparego = {'hvs': hvs_all_qparego, 'train_obj_true': train_obj_true_all_qparego, 'train_obj': train_obj_all_qparego, 
-           'train_x': train_x_all_qparego, 'method': 'qparego'} 
-
-qehvi = {'hvs': hvs_all_qehvi, 'train_obj_true': train_obj_true_all_qehvi, 'train_obj': train_obj_all_qehvi,
-         'train_x': train_x_all_qehvi, 'method': 'qehvi'}
-
-qnehvi = {'hvs': hvs_all_qnehvi, 'train_obj_true': train_obj_true_all_qnehvi, 'train_obj': train_obj_all_qnehvi,
-          'train_x': train_x_all_qnehvi, 'method': 'qnehvi'}
-dragonfly = {'hvs': hvs_all_dragonfly, 'train_obj_true': train_obj_true_all_dragonfly, 'train_obj': train_obj_all_dragonfly,
-             'train_x': train_x_all_dragonfly, 'method': 'dragonfly'}
-random = {'hvs': hvs_all_random, 'train_obj_true': train_obj_true_all_random, 'train_obj': train_obj_all_random,
-          'train_x': train_x_all_random, 'method': 'random'} 
-
-for method in [qparego, qehvi, qnehvi, dragonfly, random]:
-    json.dump(method, open(f"results/{method}.json", "w")) 
-    
-breakpoint 
